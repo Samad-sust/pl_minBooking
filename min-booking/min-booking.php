17799fac4bc2febc8cb77eddfd0e728c1485b9cb
Samad-sust <masamad.sust@gmail.com>
2021-04-23 00:56:05 +0600

    plugin created

---
+++ min-booking/min-booking.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Minimum Car Booking Days For Sunshine
+ * Version:           1.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Abdus Samad
+ * Author URI:        https://www.linkedin.com/in/samad-sust/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       Ysi General Plugin for sunshine
+ */
+if( !defined('ABSPATH')) exit;
+global $wpdb;
+
+
+
+
